eb34f3438ee1edefcb1f88ea1499e2fc55aedeba
Vaibhav Singh <61027919+vaibhav1997756@users.noreply.github.com>
2021-12-05 16:06:59 +0530

    Update sum.js

--- sum.js
+++ sum.js
@@ -1,15 +1,10 @@
 let s = parseInt(prompt("Enter first number:"));
 let d = parseInt(prompt("Enter second number:"));
 let sum = function(s) {
-
-
     let b = function(d) {
         let add = s + d;
         console.log(add);
     }
     return b;
 }
-
-
-
 sum(s)(d);
