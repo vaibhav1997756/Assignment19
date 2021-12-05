d60de2cb0f7cd82263a80bd11d6179561d3abd0b
Vaibhav Singh <61027919+vaibhav1997756@users.noreply.github.com>
2021-12-05 16:05:19 +0530

    Add files via upload

---
+++ sum.js
@@ -0,0 +1,15 @@
+let s = parseInt(prompt("Enter first number:"));
+let d = parseInt(prompt("Enter second number:"));
+let sum = function(s) {
+
+
+    let b = function(d) {
+        let add = s + d;
+        console.log(add);
+    }
+    return b;
+}
+
+
+
+sum(s)(d);
